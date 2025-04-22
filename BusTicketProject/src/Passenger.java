46de857e548131b5cc22b3d6b7e0c0c8bab955a4
htetyeemon <6731503057@lamduan.mfu.ac.th>
2025-04-22 17:30:57 +0000

    commentChanges

--- BusTicketProject/src/Passenger.java
+++ BusTicketProject/src/Passenger.java
@@ -1,14 +1,23 @@
+/*
+ Specialized Person class.
+ Demonstrates inheritance and method overriding.
+ Good practice because:
+ 1. Extends Person with passenger-specific attributes (email)
+ 2. Overrides displayDetails() to show complete information
+ 3. extensible
+ */
+
 public class Passenger extends Person {
     private String email;
 
     public Passenger(String name, int age, String email) {
-        super(name, age);
+        super(name, age);  // Reusing parent constructor
         this.email = email;
     }
 
     @Override
     public void displayDetails() {
-        super.displayDetails();
-        System.out.println("Email: " + email);
+        super.displayDetails();  // Reusing parent method
+        System.out.println("Email: " + email);  // Extending functionality
     }
 }
--- BusTicketProject/src/Person.java
+++ BusTicketProject/src/Person.java
@@ -1,5 +1,14 @@
+/*
+ Base class representing a person with common attributes.
+ Uses inheritance to allow specialization in child classes.
+ Good practice because:
+ 1. Encapsulates common person attributes (name, age)
+ 2. Provides base functionality that can be extended
+ 3. Prevents repeatition
+ */
+
 public class Person {
-    protected String name;
+    protected String name;  // Protected for child class access
     protected int age;
 
     public Person(String name, int age) {
